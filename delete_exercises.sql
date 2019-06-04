f253ff25f7c693a77997433db63320f61c92e3f1
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-06-04 15:29:45 -0500

    first exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,22 @@
 # delete all albums released after 1991
+SELECT name
+AS 'Albums released after 1991'
+FROM albums
+WHERE release_date>1991;
 DELETE FROM albums WHERE release_date>1991;
 
-# delete all disco albums
-DELETE FROM albums WHERE genre='Disco';
+# delete all disco albums\
+SELECT name
+AS 'Disco albums'
+FROM albums
+WHERE genre LIKE '%Disco%';
+DELETE FROM albums
+WHERE genre
+LIKE'%Disco%';
 
 # delete all Whitney Houston albums
-DELETE FROM albums WHERE artist='Whitney Houston';
+SELECT name AS 'Albums by Whitney Houston', artist
+FROM albums
+WHERE artist='Whitney Houston';
+DELETE FROM albums
+WHERE artist='Whitney Houston';
